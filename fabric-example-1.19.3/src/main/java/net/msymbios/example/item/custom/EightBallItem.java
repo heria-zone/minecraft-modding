29fb13c5d06fe0fc0993a402aa0041c8f9a08ac4
MSymbios <50481178+s2erge@users.noreply.github.com>
2023-01-23 11:22:56 +0000

    ADD: Tooltip to Eight Ball

--- fabric-example-1.19.3/src/main/java/net/msymbios/example/item/custom/EightBallItem.java
+++ fabric-example-1.19.3/src/main/java/net/msymbios/example/item/custom/EightBallItem.java
@@ -1,13 +1,19 @@
 package net.msymbios.example.item.custom;
 
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class EightBallItem extends Item {
 
@@ -25,6 +31,17 @@ public class EightBallItem extends Item {
         }
         return super.use(world, user, hand);
     } // use ()
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if(Screen.hasShiftDown()) {
+            tooltip.add(Text.literal("Right Click!").formatted(Formatting.AQUA));
+            tooltip.add(Text.literal("Get Random Number!").formatted(Formatting.AQUA));
+        } else {
+            tooltip.add(Text.literal("Press Shift for more Info!").formatted(Formatting.YELLOW));
+        }
+        super.appendTooltip(stack, world, tooltip, context);
+    } // appendTooltip ()
 
     private void outputRandomNumber(PlayerEntity player) {
         player.sendMessage(Text.literal("Your Number is " + getRandomNumber()));
